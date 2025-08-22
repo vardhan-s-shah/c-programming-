c5a3ba04f81351abd32ebfd63b10a551e7bbb747
vardhan-s-shah <v.s.7016437359@gmail.com>
2025-08-22 20:55:32 +0530

    Add files via upload

---
+++ . Convert dollars into pound where 1 $ = 48 Rs. And 1 pound = 70 Rs..c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	//convert dollars into pound.
+	float a;
+	printf ("write dollars:");   //1rs=0.014285 pound  
+	scanf("%f",&a);              //1rs=0.02dollar 
+	printf("pounds:%f",0.714251*a);         //0.7 pound = 1 dollar 
+	
+	
+	
+}
