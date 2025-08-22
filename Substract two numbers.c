4347912663a0de456e5869200f36281d8fc884b4
vardhan-s-shah <v.s.7016437359@gmail.com>
2025-08-22 19:28:05 +0530

    Add files via upload

---
+++ Substract two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()   {
+	
+	int a,b;
+	printf("write first number:");
+	scanf("%d",&a);
+	printf("write second number:");
+	scanf("%d",&b);
+	
+	printf("%d",a-b);
+	
+	
+}
