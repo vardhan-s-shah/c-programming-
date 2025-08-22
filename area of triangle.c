2a5c8800fde211f09e125f5ec02a8d8b39a77d7a
vardhan-s-shah <v.s.7016437359@gmail.com>
2025-08-22 23:06:02 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()  {
+	//calculate area of triangle 
+	float a,b;
+	printf("write base of triangle :");
+	scanf("%f",&a);
+	printf("write height of triangle :");
+	scanf("%f",&b);
+	
+	printf("area of triangle :%f\n",0.5*a*b);	
+	
+}
