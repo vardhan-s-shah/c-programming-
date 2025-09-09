efc8f8e4d5387402b6c5f14005fd977efabc43f6
VARDHAN S. SHAH <v.s.7016437359@gmail.com>
2025-09-09 22:59:01 +0530

    Add files via upload

---
+++ LAB 3/LAB 3 Q7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()   {
+	
+	int a,i,sum;
+	sum=0;
+	printf("write a number:");
+	scanf("%d",&a);
+	for(i=1;i<=a;i++) {   sum+=i;
+	} printf("%d",sum);
+	
+	
+	
+	
+	
+}
