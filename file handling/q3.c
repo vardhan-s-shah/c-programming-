d9a8b8489ef9900087ee896dd7a8867d0a22939d
VARDHAN S. SHAH <v.s.7016437359@gmail.com>
2025-11-11 00:16:25 +0530

    Change file name from 'output.txt' to 'example.txt'

--- file handling/q3.c
+++ file handling/q3.c
@@ -4,7 +4,7 @@ int main() {
     FILE *file;
     char line[1000];
 
-    file = fopen("output.txt", "r"); // Open file for reading
+    file = fopen("example.txt", "r"); 
     if (file == NULL) {
         printf("Error opening file.\n");
         return 1;
@@ -13,9 +13,10 @@ int main() {
     printf("Contents of the file:\n");
 
     while (fgets(line, sizeof(line), file) != NULL) {
-        printf("%s", line); // Print each line
+        printf("%s", line); 
     }
 
     fclose(file);
     return 0;
+
 }
